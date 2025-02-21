1c077c928f00803fea14088a3e521181b4b9a175
sudeakq <sudeakq@gmail.com>
2025-02-21 08:42:30 +0300

    change description

--- src/components/Menhome/Counter.js
+++ src/components/Menhome/Counter.js
@@ -3,7 +3,7 @@ import NumberCounter from 'number-counter';
 
 const counterData = [
   { count: 8, text: 'Happy Clients', imgSrc: 'assets/img/icon/cn-iocn01.png', delay: 1 },
-  { count: 2, text: 'Destinations', imgSrc: 'assets/img/icon/cn-iocn02.png', delay: 2 },
+  { count: 2, text: 'Countries', imgSrc: 'assets/img/icon/cn-iocn02.png', delay: 2 },
   { count: 0, text: 'Contracted Companies', imgSrc: 'assets/img/icon/cn-iocn03.png', delay: 1 },
   { count: 0, text: 'Producers', imgSrc: 'assets/img/icon/cn-iocn04.png', delay: 1 }
 ];
